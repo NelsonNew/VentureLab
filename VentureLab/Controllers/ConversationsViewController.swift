65176fb3a1ae6d761e75997dcddbb9081d3ce985
NelsonNew <n.neuhaeusel@gmail.com>
2021-06-06 16:03:53 +0200

    Wrap up getting started

---
+++ VentureLab/Controllers/ConversationsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  VentureLab
+//
+//  Created by Venture Lab on 06.06.21.
+//
+
+import UIKit
+
+class ConversationsViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view
+    }
+    
+}
