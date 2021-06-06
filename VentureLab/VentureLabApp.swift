249b0f38899b680abaead942f1e242c8cfce1bf5
NelsonNew <n.neuhaeusel@gmail.com>
2021-06-06 15:38:15 +0200

    Getting Started

---
+++ VentureLab/VentureLabApp.swift
@@ -0,0 +1,17 @@
+//
+//  VentureLabApp.swift
+//  VentureLab
+//
+//  Created by Venture Lab on 06.06.21.
+//
+
+import SwiftUI
+
+@main
+struct VentureLabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
